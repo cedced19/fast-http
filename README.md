94d57319a93010168f34979e9123832024ae1eb4
unknown <cedced19@gmail.com>
2014-07-24 09:50:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Reverse String
+
+[![Build Status](https://travis-ci.org/cedced19/fast-http.svg)](https://travis-ci.org/cedced19/fast-http)
+
+## Installation
+
+```
+npm install fast-http  --save
+```
+
+## Usage
+
+Create a tiny web server which does not support the MVC pattern,for simple Node.js app.
+Because I copy the same code in all my projects.
+
+## How to use
+
+~~~ javascript
+var fastHttp = require("fast-http"),
+    port = process.argv[2] || 1337;
+
+httpServer = fastHttp(port).listen(parseInt(port, 10));
+~~~
---
+++ fast-http.js
@@ -0,0 +1,48 @@
+function fastHttp (port) {
+    var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+
+  httpServer = http.createServer(function(request, response) {
+
+    var uri = url.parse(request.url).pathname
+      , filename = path.join(process.cwd(), uri);
+
+    var contentTypesByExtension = {
+      '.html': "text/html",
+      '.css':  "text/css",
+      '.js':   "text/javascript"
+    };
+
+    path.exists(filename, function(exists) {
+      if(!exists) {
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream('404.html').pipe(response);
+        return;
+      }
+
+      if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+      fs.readFile(filename, "binary", function(err, file) {
+        if(err) {
+          response.writeHead(500, {"Content-Type": "text/plain"});
+          fs.createReadStream('500.html').pipe(response);
+          return;
+        }
+
+        var headers = {};
+        var contentType = contentTypesByExtension[path.extname(filename)];
+        if (contentType) headers["Content-Type"] = contentType;
+        response.writeHead(200, headers);
+        response.write(file, "binary");
+        response.end();
+      });
+    });
+  });
+
+  return httpServer;
+  }
+
+
+module.exports = fastHttp;
