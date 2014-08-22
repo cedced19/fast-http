8d6eee8e9edfdd1ca4ade812c7298b5143b702bf
unknown <cedced19@gmail.com>
2014-08-22 11:02:03 +0200

    CLI v0.0.3

--- cli/cli.js
+++ cli/cli.js
@@ -1,18 +1,32 @@
 #!/usr/bin/env node
 'use strict';
 
-var chalk = require('chalk');
-var fastHttp = require('fast-http');
-var opn = require('opn');
-var port = process.argv.slice(2);
+var chalk = require('chalk'),
+     fastHttp = require('fast-http'),
+     opn = require('opn'),
+     port = process.argv.slice(2),
+     test = require('net').createServer();
 
 if(port.length == 0){
     console.log(chalk.red('You must enter a port!'));
     process.exit();
 }
 
+test.once('error', function(err) {
+    console.log(chalk.red('This port is already used!'));
+    process.exit();
+});
 
-fastHttp(port);
-console.log('The server has just open on ' + chalk.green('http://localhost:' + port));
-opn('http://localhost:' + port);
+test.once('listening', function() {
+  test.close();
+  fastHttp(port);
+  console.log('The server has just open on ' + chalk.green('http://localhost:' + port));
+  opn('http://localhost:' + port);
+});
+
+test.listen(port[0]);
+
+
+
+
 
