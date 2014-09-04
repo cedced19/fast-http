3a290b7cd9d56ffbae1293819233b93b691455db
unknown <cedced19@gmail.com>
2014-09-04 20:17:06 +0200

    CLI v0.0.4

--- cli/cli.js
+++ cli/cli.js
@@ -5,6 +5,8 @@ var chalk = require('chalk'),
      fastHttp = require('fast-http'),
      opn = require('opn'),
      port = process.argv.slice(2),
+     path = require('path'),
+     fs = require('fs'),
      test = require('net').createServer();
 
 if(port.length == 0){
@@ -21,7 +23,32 @@ test.once('listening', function() {
   test.close();
   fastHttp(port);
   console.log('The server has just open on ' + chalk.green('http://localhost:' + port));
+
   opn('http://localhost:' + port);
+
+  require('fb-flo')(
+          process.cwd(),
+          {
+            port: 8888,
+            host: 'localhost',
+            verbose: false,
+            glob: [
+              '**/*.js',
+              '**/*.css',
+              '**/*.html'
+            ]
+          },
+          function resolver(filepath, callback) {
+            callback({
+              resourceURL: path.extname(filepath),
+              reload: !filepath.match(/\.(css|js)$/),
+              contents: fs.readFileSync(filepath),
+              update: function(_window, _resourceURL) {
+                console.log("Resource " + _resourceURL + " has just been updated with new content");
+              }
+            });
+          }
+        );
 });
 
 test.listen(port[0]);
